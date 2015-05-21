7aa47e6c45a7faf21c1f550864bc732e5326b1e9
Ross Smith II <ross@smithii.com>
2015-05-20 20:58:14 -0700

    Update Makefile

--- version.h
+++ version.h
@@ -3,7 +3,7 @@
 #define VER_INTERNAL_NAME 		"time_it"
 #define	VER_FILE_DESCRIPTION	"Time disk I/O"
 #define VER_MAJOR				1
-#define VER_MINOR				2
-#define VER_STRING2				"1.2"
+#define VER_MINOR				3
+#define VER_STRING2				"1.3"
 
 #include "ver_defaults.h"
